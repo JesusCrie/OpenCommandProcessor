afe6bfc047bef6f66653d15bbc243ab4a0882bcf
Lucas Malandrino <lucas.malandrino@gmail.com>
2018-09-20 18:31:33 +0200

    Added parsing for short & long options

--- src/main/java/com/jesus_crie/open_command_processor/processor/CommandProcessor.java
+++ src/main/java/com/jesus_crie/open_command_processor/processor/CommandProcessor.java
@@ -12,20 +12,54 @@ public class CommandProcessor {
     private static final char QUOTE_SINGLE = '\'';
     private static final char QUOTE_DOUBLE = '"';
     private static final char OPTION_PREFIX = '-';
-    private static final Pattern PATTERN_CHAR_ALLOWED = Pattern.compile("[a-zA-Z0-9-_]");
+    private static final Pattern PATTERN_OPTION_ALLOWED = Pattern.compile("[a-zA-Z0-9-_]");
+    private static final Pattern PATTERN_OPTION_SHORT_ALLOWED = Pattern.compile("[a-zA-Z0-9]");
 
     public ProcessedResult process(final CommandContext context, final String input) {
-        if (input.length() == 0)
+        if (input == null || input.length() == 0)
             throw new IllegalArgumentException("The input string is empty !");
 
         final Cursor cursor = new Cursor(input.trim());
 
-        // First there must be the name of the command
+        final ProcessedResult.Builder builder = new ProcessedResult.Builder();
+        builder.setCommandName(readWord(cursor));
 
+        char c;
+        boolean wasPrefix = false;
+
+        while (cursor.hasNext()) {
+            c = cursor.read();
+
+            // if there was a prefix before and now another one
+            if (wasPrefix && c == OPTION_PREFIX) {
+                builder.appendLongOption(readLongOption(cursor));
+                wasPrefix = false;
+
+                // if there was a single prefix before and now another character
+            } else if (wasPrefix) {
+                for (char op : readShortOptions(cursor))
+                    builder.appendShortOption(op);
+                wasPrefix = false;
+
+                // An option prefix with nothing before
+            } else if (c == OPTION_PREFIX) {
+                wasPrefix = true;
+
+                // The start of a quoted word
+            } else if (c == QUOTE_DOUBLE || c == QUOTE_SINGLE) {
+                builder.appendToPath(readQuotedString(cursor, c));
+            }
+        }
 
         return null;
     }
 
+    /**
+     * Read a simple word, until the next whitespace. No quote allowed, escape characters are working.
+     *
+     * @param cursor The current cursor.
+     * @return The next word in the cursor.
+     */
     public String readWord(final Cursor cursor) {
         final StringBuilder out = new StringBuilder();
 
@@ -50,9 +84,6 @@ public class CommandProcessor {
     /**
      * Read and return the next character, ignoring it's nature.
      * Typically called for the character after the escape character (\).
-     * <p>
-     * Ex: "hey \" "
-     *           ^
      *
      * @param cursor The cursor.
      * @return The next character.
@@ -68,9 +99,6 @@ public class CommandProcessor {
 
     /**
      * Read and return the string contained between 2 characters, typically a quoted string.
-     * <p>
-     * Ex: "hey 'dude'"
-     *      ^^^^^^^^^^
      *
      * @param cursor  The cursor.
      * @param endChar The character denoting the end of the quote.
@@ -78,20 +106,68 @@ public class CommandProcessor {
      */
     public String readQuotedString(final Cursor cursor, final char endChar) {
         final StringBuilder out = new StringBuilder();
+        cursor.newEra();
 
         char c;
-        try {
-            while ((c = cursor.read()) != endChar) {
-                if (c == ESCAPE_CHARACTER)
-                    out.append(readEscapedCharacter(cursor));
-                else
-                    out.append(c);
-            }
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new CommandProcessingException("Quote not closed !", cursor.eraStart, cursor.position);
+        while (cursor.hasNext()) {
+            c = cursor.read();
+
+            if (c == ESCAPE_CHARACTER)
+                out.append(readEscapedCharacter(cursor));
+            else if (c == endChar)
+                return out.toString();
+            else
+                out.append(c);
         }
 
         return out.toString();
+    }
+
+    /**
+     * Read a long option from the cursor, a word with only alphanumerical characters.
+     *
+     * @param cursor The targeted cursor.
+     * @return The option that was parsed from the cursor.
+     * @throws CommandProcessingException If the option contains illegal character(s).
+     */
+    public String readLongOption(final Cursor cursor) {
+        final String option = readWord(cursor);
+
+        if (!PATTERN_OPTION_ALLOWED.matcher(option).matches()) {
+            throw new CommandProcessingException("Illegal character found in long option !", cursor.eraStart, cursor.position);
+        }
+
+        return option;
+    }
+
+    /**
+     * Read one or more short options from the cursor.
+     *
+     * @param cursor The source cursor.
+     * @return All of the short options contained in the cursor.
+     * @throws CommandProcessingException If an illegal character is found in one of the options.
+     */
+    private char[] readShortOptions(final Cursor cursor) {
+        char first = cursor.get();
+
+        char[] options;
+        if (cursor.hasNext() && cursor.getNext() != SPACE_CHARACTER) {
+            final String ops = readLongOption(cursor); // Not very clean but it's the exact same thing that
+
+            if (!PATTERN_OPTION_SHORT_ALLOWED.matcher(ops).matches()) {
+                throw new CommandProcessingException("Illegal character found in short option !", cursor.eraStart, cursor.position);
+            }
+
+            options = new char[ops.length() + 1];
+            options[0] = first;
+            for (int i = 0; i < ops.toCharArray().length; i++)
+                options[i + 1] = ops.charAt(i);
+
+        } else {
+            options = new char[]{first};
+        }
+
+        return options;
     }
 
     public Object suggest(final CommandContext context, final String input) {
@@ -99,7 +175,7 @@ public class CommandProcessor {
     }
 
     /**
-     * A useful class to walk through a string step by step and eventually divide it in eras.
+     * A internal class to walk through a string step by step and eventually divide it in eras.
      */
     static class Cursor {
 
@@ -111,33 +187,77 @@ public class CommandProcessor {
             this.payload = payload;
         }
 
+        /**
+         * @return True if {@link #read()} can be called without risk.
+         */
         public boolean hasNext() {
             return position < payload.length() - 1;
         }
 
+        /**
+         * @return The next character of the payload.
+         * @throws StringIndexOutOfBoundsException If there is no character left.
+         */
         public char read() {
             position++;
             return get();
         }
 
+        /**
+         * Used to retrieve the character under the previously selected position.
+         * Mainly used by {@link #read()}.
+         *
+         * @return The character under the current position.
+         * @throws StringIndexOutOfBoundsException If the cursor is positioned after or before the payload.
+         */
         public char get() {
             return payload.charAt(position);
         }
 
+        /**
+         * @return The character just before the position of the cursor.
+         */
+        public char getPrevious() {
+            return payload.charAt(position - 1);
+        }
+
+        /**
+         * @return The character just ahead of the current cursor.
+         */
+        public char getNext() {
+            return payload.charAt(position + 1);
+        }
+
+        /**
+         * @return The current position of the cursor on the payload.
+         */
         public int getPosition() {
             return position;
         }
 
+        /**
+         * Start a new era on the current cursor. Typically when we switch from a word to another.
+         * Used to mark the start of the part that is currently parsed.
+         * <p>
+         * The era will start one character ahead of the current position.
+         */
         public void newEra() {
-            eraStart = position;
+            eraStart = position + 1;
         }
 
+        /**
+         * @return The starting position of the current era.
+         */
         public int getEraStart() {
             return eraStart;
         }
 
+        /**
+         * Reset the cursor and the era to the starting position.
+         */
         public void reset() {
-            position = eraStart = 0;
+            position = -1;
+            eraStart = 0;
         }
     }
 }
--- src/main/java/com/jesus_crie/open_command_processor/processor/ProcessedResult.java
+++ src/main/java/com/jesus_crie/open_command_processor/processor/ProcessedResult.java
@@ -1,6 +1,10 @@
 package com.jesus_crie.open_command_processor.processor;
 
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public abstract class ProcessedResult {
 
     public abstract boolean isErrored();
@@ -10,4 +14,32 @@ public abstract class ProcessedResult {
     public abstract boolean optionHasArgument(String arg);
 
     public abstract String[] getCompletePath();
+
+    static class Builder {
+
+        private String cmdName;
+        private final LinkedList<String> path = new LinkedList<>();
+        private final List<String> options = new ArrayList<>();
+        private final List<Character> shortOptions = new ArrayList<>();
+
+        public Builder setCommandName(final String cmdName) {
+            this.cmdName = cmdName;
+            return this;
+        }
+
+        public Builder appendToPath(final String path) {
+            return this;
+        }
+
+        public Builder appendLongOption(final String longOption) {
+            options.add(longOption);
+            // TODO 9/20/18 check if the option take an argument and use the next path as the argument
+            return this;
+        }
+
+        public Builder appendShortOption(final char op) {
+            shortOptions.add(op);
+            return this;
+        }
+    }
 }
